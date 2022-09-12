ae360d991a937685456b921d3f80a5c68d02975e
Sass <dudu_sass12@hotmail.com>
2022-09-12 15:45:34 -0300

    feat:inventory

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,17 +1,35 @@
 import csv
+import json
+from xml.etree import ElementTree as ET
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
 
 class Inventory:
-    def read_csv(path):
-        with open(path) as file:
-            dict_list = csv.DictReader(file)
-            return list(dict_list)
+    def read(path):
+        if path.endswith(".csv"):
+            with open(path) as file:
+                dict_list = csv.DictReader(file)
+                return list(dict_list)
+
+        elif path.endswith(".json"):
+            with open(path) as file:
+                dict_list = json.loads(file.read())
+            return dict_list
+
+        elif path.endswith(".xml"):
+            if path.endswith(".xml"):
+                with open(path) as file:
+                    tree = ET.parse(file)
+                    root = tree.getroot()
+                return list(
+                    {item.tag: item.text for item in row} for row in root
+                    )
+        return list()
 
     @classmethod
     def import_data(cls, path, type):
-        data = Inventory.read_csv(path)
+        data = Inventory.read(path)
         if type == "simples":
             return SimpleReport.generate(data)
         elif type == "completo":
